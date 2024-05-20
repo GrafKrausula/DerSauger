51473306f41b818b93d7dbe8b941eee7d7d5ebc2
GrafKrausula <50548177+GrafKrausula@users.noreply.github.com>
2024-05-20 22:08:07 +0200

    Update background.js

--- Der Sauger/Sauger Chrome Extension/background.js
+++ Der Sauger/Sauger Chrome Extension/background.js
@@ -6,14 +6,13 @@ var convert = null;
 var downloadpath = null;
 var convertpath = null;
 
-var getKeys = function(obj){
+var getKeys = function(obj) {
    var keys = [];
    for(var key in obj){
       keys.push(key);
    }
    return keys;
 }
-
 
 function onNativeMessage(message) {
   console.log("Received message: <b>" + JSON.stringify(message) + "</b>");
@@ -35,95 +34,76 @@ async function connect() {
 }
 
 async function sendNativeMessage(message) {
-
   await port.postMessage(message);
   console.log("Sent message: <b>" + JSON.stringify(message) + "</b>");
 }
 
 async function send(message) {
   console.log("Python script running: " + connected);
-  if(!connected){
+  if (!connected) {
     await connect();
   }
   await sendNativeMessage(message);
 }
 
 async function sendPreferences() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get([
-    "favouriteFormat", //das rote ist der speicherkey, das weißte die zu speichernde variable
-    "wantsConvert",
-    'CNVtpath',
-    'DLpath'],
-    async function(items) {
+  chrome.storage.sync.get(["favouriteFormat", "wantsConvert", 'CNVtpath', 'DLpath'], async function(items) {
     format = items.favouriteFormat;
     convert = items.wantsConvert;
     downloadpath = items.DLpath;
     convertpath = items.CNVtpath;
-    message = {format, convert, downloadpath, convertpath};
-    await send(message)
-    getAndSendUrl()
+    let message = { format, convert, downloadpath, convertpath };
+    await send(message);
+    getAndSendUrl();
   });
 }
 
-
 async function sendFixedPreferences(command) {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get([
-    "favouriteFormat", //das rote ist der speicherkey, das weißte die zu speichernde variable
-    "wantsConvert",
-    'CNVtpath',
-    'DLpath'],
-    async function(items) {
+  chrome.storage.sync.get(["favouriteFormat", "wantsConvert", 'CNVtpath', 'DLpath'], async function(items) {
     format = command;
     convert = items.wantsConvert;
     downloadpath = items.DLpath;
     convertpath = items.CNVtpath;
-    message = {format, convert, downloadpath, convertpath};
-    await send(message)
-    getAndSendUrl()
+    let message = { format, convert, downloadpath, convertpath };
+    await send(message);
+    getAndSendUrl();
   });
 }
 
-async function invokeUpdate(){
-  await send("invokeUpdate")
+async function invokeUpdate() {
+  await send("invokeUpdate");
 }
 
-async function getAndSendUrl(){
-  chrome.tabs.query({active: true, currentWindow: true}, async tabs => {
+async function getAndSendUrl() {
+  chrome.tabs.query({ active: true, currentWindow: true }, async tabs => {
     var url = tabs[0].url;
     console.log(url); //Die konsole beweißt, die url wird richtig abgerufen
-    message = {"url": url}; //Hier url von momentanen tab
-    await send(message)
-
+    let message = { "url": url }; //Hier url von momentanen tab
+    await send(message);
   });
 }
 
 chrome.commands.onCommand.addListener(async function(command) {
-  if(command == "toggle"){
-    sendPreferences() //sendet url und präferenzen
+  if (command == "toggle") {
+    sendPreferences(); //sendet url und präferenzen
   }
-  if(command == "wav"){
-    sendFixedPreferences(command) //sendet url und präferenzen
+  if (command == "wav") {
+    sendFixedPreferences(command); //sendet url und präferenzen
   }
-  if(command == "mp3"){
-    sendFixedPreferences(command) //sendet url und präferenzen
+  if (command == "mp3") {
+    sendFixedPreferences(command); //sendet url und präferenzen
   }
-  if(command == "mp4"){
-    sendFixedPreferences(command) //sendet url und präferenzen
+  if (command == "mp4") {
+    sendFixedPreferences(command); //sendet url und präferenzen
   }
 });
 
-chrome.browserAction.onClicked.addListener(async function (tab) {
-  //chrome.tabs.create({url: 'main.html'})
-  sendPreferences() //sendet url und präfernezen
+chrome.action.onClicked.addListener(async function (tab) {
+  sendPreferences(); //sendet url und präferenzen
+});
 
-})
-
-
-chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
-{
-    if( request.Message === "UPDATE" ){
-      invokeUpdate()
-    }
-})
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.Message === "UPDATE") {
+    invokeUpdate();
+  }
+});
